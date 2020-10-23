8ee36c7ee8add5a10203c4c36f7d0b8337679df8
Yizhen Zhao <yz@ark7.com>
2020-10-22 20:51:33 -0700

    Change test.

--- test/metadata.ts
+++ test/metadata.ts
@@ -6,7 +6,9 @@ import { Name } from './models';
 import { metadata } from './metadata/name';
 
 describe('metadata', () => {
-  it('should return expected value for Name', () => {
-    A7Model.getMetadata(Name).should.have.properties(metadata.name);
+  describe('models', () => {
+    it('should return expected value for Name', () => {
+      A7Model.getMetadata(Name).should.have.properties(metadata.name);
+    });
   });
 });
