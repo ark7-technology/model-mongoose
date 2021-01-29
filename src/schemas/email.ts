50f9bcdb017612694fe53e00357ce5f7339ac18e
ray <r@ark7.com>
2021-01-28 23:42:46 -0800

    support email as well

--- src/schemas/email.ts
+++ src/schemas/email.ts
@@ -14,7 +14,12 @@ export class Email extends BaseSchemaType {
     return /^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(val);
   }
 
-  cast(val: any) {
+  cast(val: any, options :any) {
+    // Allow direct pass of regex in query.
+    if (options instanceof mongoose.Query && val instanceof RegExp) {
+      return val;
+    }
+    
     if (val.constructor !== String) {
       throw new (mongoose.SchemaType as any).CastError(
         'Email',
