70aa81dccc235996a9f083df1e8190487cbc4b9b
Yizhen Zhao <yz@ark7.com>
2023-03-12 14:48:13 -0700

    Fix the reference.

--- src/schemas/object-id.ts
+++ src/schemas/object-id.ts
@@ -1,5 +1,5 @@
+import * as mongoose from 'mongoose';
 import _ from 'underscore';
-import mongoose from 'mongoose';
 
 mongoose.Schema.Types.ObjectId.cast((v: any) => {
   return v === '' ? null : _.isString(v) ? new mongoose.Types.ObjectId(v) : v;
