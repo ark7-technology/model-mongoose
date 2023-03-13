a4504376aca925d094c0dcca201b585b579ddae2
Yizhen Zhao <yz@ark7.com>
2023-03-12 21:21:29 -0700

    Fix ObjectId cast.

--- src/schemas/object-id.ts
+++ src/schemas/object-id.ts
@@ -2,5 +2,13 @@ import * as mongoose from 'mongoose';
 import _ from 'underscore';
 
 mongoose.Schema.Types.ObjectId.cast((v: any) => {
-  return v === '' ? null : _.isString(v) ? new mongoose.Types.ObjectId(v) : v;
+  return v == null || v === ''
+    ? null
+    : _.isString(v)
+    ? new mongoose.Types.ObjectId(v)
+    : v._id != null
+    ? _.isString(v._id)
+      ? new mongoose.Types.ObjectId(v._id)
+      : v._id
+    : v;
 });
